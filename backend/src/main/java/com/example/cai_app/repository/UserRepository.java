73726e701fd31f89549d6545d32cd0302dc29677
hj667 <hongji667719@gmail.com>
2025-02-24 14:36:11 -0800

    updated frontend

--- backend/src/main/java/com/example/cai_app/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.cai_app.repository;
-import com.example.cai_app.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-    User findByName(String userName);
-}
